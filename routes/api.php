246625e671ff8dd174cfd99559c30261647866ae
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-22 19:39:51 -0500

    routes controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FrontController;
+use App\Http\Controllers\Api\Admin\CategoryController;
+use App\Http\Controllers\Api\Admin\EmpresaController;
+use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Api\Client\EmpresaController as EmpresaClientController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +36,7 @@ Route::prefix('v1')->group(function(){
         Route::post('/auth/logout',[AuthController::class, 'logout']);
 
         //::rol client
-        Route::apiResource('/client/empresa',EmpresaController::class);
+        Route::apiResource('/client/empresa',EmpresaClientController::class);
 
         //::rol admin
         Route::apiResource('/admin/user',UserController::class);
