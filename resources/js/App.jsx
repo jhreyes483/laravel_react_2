981796cc09f0d87d1c0b29d13aa3a57238d8ff33
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-22 19:09:37 -0500

    configuracion de web para que todas las vistas cargen en welcome

---
+++ resources/js/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>App
+
+        <button className='btn btn-success'>Boton</button>
+    </div>
+  )
+}
+
+export default App
+
+if (document.getElementById('root')) {
+    const Index = ReactDOM.createRoot(document.getElementById("root"));
+
+    Index.render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    )
+}
--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -7,7 +7,7 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">BIENVENIDO CODE APP</div>
 
                         <div className="card-body">I'm an example component!</div>
                     </div>
@@ -19,8 +19,8 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    const Index = ReactDOM.createRoot(document.getElementById("example"));
+if (document.getElementById('root')) {
+    const Index = ReactDOM.createRoot(document.getElementById("root"));
 
     Index.render(
         <React.StrictMode>
